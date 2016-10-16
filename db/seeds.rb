7874978027b72b51dfb791b82b4ff82ffe50125e
annedien <annedient@gmail.com>
2016-10-16 09:52:04 +0200

    crate song model and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+song1 = Song.create ( { name: "I can get no satisfaction"} )
+song2 = Song.create ( { name: "Ne me quitte pas"} )
+song3 = Song.create  ( { name: "Kiss"} )
