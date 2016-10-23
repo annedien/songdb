923cc8a245640480bc54e536905373f29aa0a75f
annedien <annedient@gmail.com>
2016-10-23 19:08:47 +0200

    added templating engines

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  config.generators do |g|
-    g.javascript_engine = :js
-    g.stylesheet_engine = :scss
-    g.template_engine = :erb
-  end
 end
+
--- config/application.rb
+++ config/application.rb
@@ -6,13 +6,15 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Songdb
+module SongdbAjax
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    console do
-      ActiveRecord::Base.connection
+    config.generators do |g|
+      g.javascript_engine = :js
+      g.stylesheet_engine = :scss
+      g.template_engine = :erb
     end
   end
 end
