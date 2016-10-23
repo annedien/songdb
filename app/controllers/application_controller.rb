014576e21578969ba85e76b1094b2ec05731fb1d
annedien <annedient@gmail.com>
2016-10-23 17:42:31 +0200

    adding template engines in appcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  config.generators do |g|
+    g.javascript_engine = :js
+    g.stylesheet_engine = :scss
+    g.template_engine = :erb
+  end
 end
