9008816cd56114c1b34b741d8e6545f3a3f0a281
annedien <annedient@gmail.com>
2016-10-23 20:05:18 +0200

    added def update and destroy for artistcontroller

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -19,4 +19,32 @@ class ArtistsController < ApplicationController
       end
     end
   end
+
+  def update
+    respond_to do |format|
+      if @artist.update(song_params)
+        format.html { redirect_to @artist, notice: 'Song was successfully updated.' }
+        format.json { render :show, status: :ok, location: @todo }
+      else
+        format.html { render :edit }
+        format.json { render json: @song.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @songs.destroy
+    respond_to do |format|
+      format.html { redirect_to @artist, notice: 'Songs were successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  def song_params
+    params.require(:artist).permit(:song)
+  end
+
 end
+
+
